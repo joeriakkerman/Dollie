24888da686fc0fb743b85683e5cb80ca852e4896
rbernies <34214367+rbernies@users.noreply.github.com>
2019-03-20 12:54:36 +0100

    push before new branch

--- resources/views/accounts.blade.php
+++ resources/views/accounts.blade.php
@@ -1,9 +1,28 @@
 @extends('layouts.app')
 
+
+
 @section('content')
+
 <div class="container">
-    <div class="row justify-content-center">
-        This will be the billing accounts page
-    </div>
+<table class="table">
+<tr>
+<th>Bankrekeningnummer</th>
+<th>Saldo</th>
+</tr>
+<tr>
+<td>NL58RABO2421442</td>
+<td>15MILJOEN EUROS</td>
+</tr>
+</table>
+</div>
+
+<div class="container">
+<form action="/action_page.php">
+  Rekeningnummer:<br>
+  <input type="text" name="firstname" value="Mickey">
+  <br>
+  <input type="submit" value="Submit">
+</form> 
 </div>
 @endsection
