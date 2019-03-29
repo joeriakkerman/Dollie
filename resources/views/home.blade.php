cb2abd3648c8ef3bde59606d875e2c2e1e069174
joeriakkerman <akkermanjoeri@gmail.com>
2019-03-29 14:43:49 +0100

    Changed copy link system, now it uses the actual routing system to create the link (so when we use ngrok, it has the right link)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -87,7 +87,7 @@
                         function copyLink(id){
                             var tempInput = document.createElement("input");
                             tempInput.style = "position: absolute; left: -1000px; top: -1000px";
-                            tempInput.value = "dollie.local/payment/?dollie_id=" + id;
+                            tempInput.value = "{{ route('payment.link') }}/?dollie_id=" + id;
                             document.body.appendChild(tempInput);
                             tempInput.select();
                             document.execCommand("copy");
